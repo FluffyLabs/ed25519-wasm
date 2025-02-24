97b0a406dbaf470565e68f290dd9a009be5ce2b9
Tomasz Drwięga <tomusdrw@gmail.com>
2025-02-24 22:08:06 +0100

    Refactor code.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
-use ed25519_dalek::{Signature, VerifyingKey};
-use std::io::{Cursor, Read, Result};
+use ed25519_dalek::{ed25519, Signature, VerifyingKey};
+use std::io::{self, Cursor, Read};
 use wasm_bindgen::prelude::wasm_bindgen;
+
+const KEY_LENGTH: usize = 32;
+const SIGNATURE_LENGTH: usize = 64;
+
+type Message = Vec<u8>;
 
 /**
  * Verify Ed25519 signatures one by one using strict verification.
@@ -15,44 +20,13 @@ pub fn verify_ed25519(
     let mut cursor = Cursor::new(data);
 
     while cursor.position() < data.len() as u64 {
-        let maybe_key = read_key(&mut cursor);
-        let maybe_signature = read_signature(&mut cursor);
-        let maybe_message_length = read_message_length(&mut cursor);
+        let verificaton_result = read_chunk(&mut cursor)
+            .and_then(|(key, signature, message)| Ok(key.verify_strict(&message, &signature)?));
 
-        let (maybe_verifying_key, signature, message_len) =
-            match (maybe_key, maybe_signature, maybe_message_length) {
-                (Ok(key), Ok(signature), Ok(message_len)) => {
-                    (VerifyingKey::from_bytes(&key), signature, message_len)
-                }
-                (_, Err(e), _) => {
-                    println!("pppp, {}", e);
-                    results.push(0);
-                    continue;
-                }
-                _ => {
-                    println!("ttt");
-                    results.push(0);
-                    continue;
-                }
-            };
-
-        let maybe_message = read_message(&mut cursor, message_len as usize);
-        let (verifying_key, message) = match (maybe_verifying_key, maybe_message) {
-            (Ok(key), Ok(message)) => (key, message),
-            _ => {
-                println!("");
-                results.push(0);
-                continue;
-            }
-        };
-
-        let verification_result: u8 =
-            match verifying_key.verify_strict(&message, &Signature::from_bytes(&signature)) {
-                Ok(_) => 1,
-                Err(_) => 0,
-            };
-
-        results.push(verification_result);
+        results.push(match verificaton_result {
+            Ok(_) => 1,
+            Err(_) => 0,
+        });
     }
 
     results
@@ -69,67 +43,56 @@ pub fn verify_ed25519_batch(
     data: &[u8], // [key (32 bytes), signature (64 bytes), message_length (1 byte) message (message_length bytes)]
 ) -> bool {
     let mut cursor = Cursor::new(data);
-    let mut messages = vec![];
-    let mut signatures = vec![];
     let mut keys = vec![];
+    let mut signatures = vec![];
+    let mut messages = vec![];
     while cursor.position() < data.len() as u64 {
-        let maybe_key = read_key(&mut cursor);
-        let maybe_signature = read_signature(&mut cursor);
-        let maybe_message_length = read_message_length(&mut cursor);
+        let chunk = read_chunk(&mut cursor);
 
-        let (maybe_verifying_key, signature, message_len) =
-            match (maybe_key, maybe_signature, maybe_message_length) {
-                (Ok(key), Ok(signature), Ok(message_len)) => {
-                    (VerifyingKey::from_bytes(&key), signature, message_len)
-                }
-                _ => {
-                    continue;
-                }
-            };
-
-        let maybe_message = read_message(&mut cursor, message_len as usize);
-        let (verifying_key, message) = match (maybe_verifying_key, maybe_message) {
-            (Ok(key), Ok(message)) => (key, message),
-            _ => {
-                continue;
-            }
-        };
-
-        messages.push(message);
-        signatures.push(Signature::from_bytes(&signature));
-        keys.push(verifying_key);
+        if let Ok((key, signature, message)) = chunk {
+            keys.push(key);
+            signatures.push(signature);
+            messages.push(message);
+        } else {
+            return false;
+        }
     }
 
     let messages_refs: Vec<&[u8]> = messages.iter().map(|msg| msg.as_slice()).collect();
-    match ed25519_dalek::verify_batch(&messages_refs, &signatures, &keys) {
-        Ok(_) => true,
-        Err(_) => false,
+
+    ed25519_dalek::verify_batch(&messages_refs, &signatures, &keys).is_ok()
+}
+
+fn read_chunk(cursor: &mut Cursor<&[u8]>) -> Result<(VerifyingKey, Signature, Message), Error> {
+    let mut key = [0u8; KEY_LENGTH];
+    let mut sig = [0u8; SIGNATURE_LENGTH];
+    let mut msg_len = [0u8; 1];
+    cursor.read_exact(&mut key)?;
+    cursor.read_exact(&mut sig)?;
+    cursor.read_exact(&mut msg_len)?;
+
+    let mut msg = vec![0u8; msg_len[0] as usize];
+    cursor.read_exact(&mut msg)?;
+    Ok((
+        VerifyingKey::from_bytes(&key)?,
+        Signature::from_bytes(&sig),
+        msg,
+    ))
+}
+
+pub enum Error {
+    Io(io::Error),
+    Crypto(ed25519::Error),
+}
+
+impl From<io::Error> for Error {
+    fn from(value: io::Error) -> Self {
+        Self::Io(value)
     }
 }
 
-const KEY_LENGTH: usize = 32;
-const SIGNATURE_LENGTH: usize = 64;
-
-fn read_key(cursor: &mut Cursor<&[u8]>) -> Result<[u8; KEY_LENGTH]> {
-    let mut buf = [0u8; KEY_LENGTH];
-    cursor.read_exact(&mut buf)?;
-    Ok(buf)
-}
-
-fn read_signature(cursor: &mut Cursor<&[u8]>) -> Result<[u8; SIGNATURE_LENGTH]> {
-    let mut buf = [0u8; SIGNATURE_LENGTH];
-    cursor.read_exact(&mut buf)?;
-    Ok(buf)
-}
-
-fn read_message_length(cursor: &mut Cursor<&[u8]>) -> Result<u8> {
-    let mut buf = [0u8; 1];
-    cursor.read_exact(&mut buf)?;
-    Ok(buf[0])
-}
-
-fn read_message(cursor: &mut Cursor<&[u8]>, length: usize) -> Result<Vec<u8>> {
-    let mut buf = vec![0u8; length];
-    cursor.read_exact(&mut buf)?;
-    Ok(buf)
+impl From<ed25519::Error> for Error {
+    fn from(value: ed25519::Error) -> Self {
+        Self::Crypto(value)
+    }
 }
